da250934228d149851f743a20681d4788d235586
chlthgus <fps8295@naver.com>
2025-03-31 17:59:52 +0900

    20250331
    .

---
+++ Assets/_Sample/04RotatTest/MachineGunTower.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class MachineGunTower : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+    private void UpdateTarget()
+    {
+      //  GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+
+       // foreach (var enemy in enemies)
+      //  {
+      //      float distance = Vector3.Distance(this.transform.position, enemy.transform);
+      //  }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
